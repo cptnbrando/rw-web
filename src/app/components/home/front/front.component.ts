59f8c60ee78b7f318fdf5880f0fe4f9a27e4cc49
cptnbrando <brandon.dcruz@revature.net>
2021-09-23 16:15:39 -0500

    Added sakurajs

---
+++ src/app/components/home/front/front.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var Sakura: any;
+
+@Component({
+	selector: 'app-front',
+	templateUrl: './front.component.html',
+	styleUrls: ['./front.component.scss'],
+})
+export class FrontComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {
+		const sakura = new Sakura('#front', {
+			maxSize: 50,
+			minSize: 20,
+            colors: [
+                {
+                    gradientColorStart: 'rgba(255, 183, 197, 1)',
+                    gradientColorEnd: 'rgba(255, 197, 208, 0.9)',
+                    gradientColorDegree: 120,
+                },
+                {
+                    gradientColorStart: 'rgba(255, 255, 255, 0.8)',
+                    gradientColorEnd: 'rgba(255, 20, 147, 0.8)',
+                    gradientColorDegree: 60,
+                },
+                {
+                    gradientColorStart: 'rgb(212, 152, 163)',
+                    gradientColorEnd: 'rgb(242, 185, 196)',
+                    gradientColorDegree: 120,
+                },
+                {
+                    gradientColorStart: 'rgba(255, 192, 0, 0.5)',
+                    gradientColorEnd: 'rgba(255, 189, 189, 0.9)',
+                    gradientColorDegree: 60,
+                },
+            ],
+		});
+	}
+}
--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { NavbarModule } from '../navbar/navbar.module';
+import { FrontComponent } from './front/front.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    FrontComponent
   ],
   imports: [
     CommonModule,
